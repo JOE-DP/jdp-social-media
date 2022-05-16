06c060578013e1735457b1414b40e9fb7f743b50
JOE-DP <joe.purvis@hotmail.co.uk>
2022-05-16 16:40:44 +0100

    general progress across app - functional auth still outstanding

--- routes/homeroutes.js
+++ routes/homeroutes.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const homecontroller = require('../controllers/homecontroller')
 
+// const { ensureAuth, ensureGuest } = require('../middleware/auth')
+
 router.get('/', homecontroller.homeGet)
 
 module.exports = router
--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const mongoose = require('mongoose')
 const MongoStore = require('connect-mongo')
 const connectDB = require('./config/database')
 
+const indexroutes = require('./routes/indexroutes')
+const homeroutes = require('./routes/homeroutes')
+const authroutes = require('./routes/authroutes')
 
 
 
@@ -22,7 +25,7 @@ app.use(express.json())
 // Sessions
 app.use(
     session({
-      secret: 'applebob',
+      secret: 'akeyboard cat',
       resave: false,
       saveUninitialized: false,
       store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
@@ -40,13 +43,9 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 //routes
-const indexroutes = require('./routes/indexroutes')
+
 app.use('/', indexroutes)
-
-const homeroutes = require('./routes/homeroutes')
 app.use('/home', homeroutes)
-
-const authroutes = require('./routes/authroutes')
 app.use('/auth', authroutes)
 
 
