0f1090d17c513504120e8b760c3efa28a96b5c6f
JOE-DP <joe.purvis@hotmail.co.uk>
2022-05-17 19:03:07 +0100

    heroku changes

--- config/passport.js
+++ config/passport.js
@@ -2,7 +2,7 @@ const OIDCStrategy = require('passport-azure-ad').OIDCStrategy
 
 const mongoose = require('mongoose')
 const config = require('../config/config')
-const User = require('../models/User')
+const User = require('/app/models/User')
 
 
 module.exports = function (passport) {
